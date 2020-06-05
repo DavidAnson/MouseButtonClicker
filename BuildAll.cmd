8f7604e56def94a744a6a1aa7cac4f0a841299fd
David Anson <david@dlaa.me>
2020-06-04 21:01:27 -0700

    Create simple x86/x64 build script based on Build Tools for Visual Studio 2019 and Windows 10 SDK.

---
+++ BuildAll.cmd
@@ -0,0 +1,10 @@
+call :BUILD x86
+call :BUILD x64
+goto :EOF
+
+:BUILD
+mkdir %1
+del %1\MouseButtonClicker.*
+"%WindowsSdkVerBinPath%%1\rc" /fo %1\MouseButtonClicker.res MouseButtonClicker.rc
+"%VCToolsInstallDir%bin\Hostx86\%1\cl" MouseButtonClicker.cpp %1\MouseButtonClicker.res /Fo%1\MouseButtonClicker.obj /Fe%1\MouseButtonClicker.exe /link user32.lib /LIBPATH:"%VCToolsInstallDir%lib\%1" /LIBPATH:"%WindowsSdkDir%Lib\%WindowsSDKLibVersion%ucrt\%1" /LIBPATH:"%WindowsSdkDir%Lib\%WindowsSDKLibVersion%um\%1"
+goto :EOF
