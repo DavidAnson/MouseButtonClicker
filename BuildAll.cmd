f343ff93542b815acf9d4102ed2355c6d04849fb
David Anson <david@dlaa.me>
2020-06-07 15:09:55 -0700

    Add compiler switches for small code, additional security checks/warnings, UTF-8 encoding, and no debug info.

--- BuildAll.cmd
+++ BuildAll.cmd
@@ -6,5 +6,5 @@ goto :EOF
 mkdir %1
 del %1\MouseButtonClicker.*
 "%WindowsSdkVerBinPath%%1\rc" /fo %1\MouseButtonClicker.res MouseButtonClicker.rc
-"%VCToolsInstallDir%bin\Hostx86\%1\cl" MouseButtonClicker.cpp %1\MouseButtonClicker.res /Fo%1\MouseButtonClicker.obj /Fe%1\MouseButtonClicker.exe /link user32.lib /LIBPATH:"%VCToolsInstallDir%lib\%1" /LIBPATH:"%WindowsSdkDir%Lib\%WindowsSDKLibVersion%ucrt\%1" /LIBPATH:"%WindowsSdkDir%Lib\%WindowsSDKLibVersion%um\%1"
+"%VCToolsInstallDir%bin\Hostx86\%1\cl" MouseButtonClicker.cpp %1\MouseButtonClicker.res /O1 /sdl /W4 /utf-8 /Fo%1\MouseButtonClicker.obj /Fe%1\MouseButtonClicker.exe /link user32.lib /LIBPATH:"%VCToolsInstallDir%lib\%1" /LIBPATH:"%WindowsSdkDir%Lib\%WindowsSDKLibVersion%ucrt\%1" /LIBPATH:"%WindowsSdkDir%Lib\%WindowsSDKLibVersion%um\%1" /NOCOFFGRPINFO
 goto :EOF
